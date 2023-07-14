ce70f22567a541f7e03fc083c904aeed347a28df
QIZHAO CHEN <2766657C@student.gla.ac.uk>
2023-07-14 04:10:54 +0100

    update PingpongController

--- backend/src/main/java/com/amigoscode/PingpongController.java
+++ backend/src/main/java/com/amigoscode/PingpongController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PingPongController {
+public class PingpongController {
 
-    record PingPong(String result) { }
+    record Pingpong(String result) { }
 
     @GetMapping("/ping")
-    public PingPong getPingPong(){
-        return new PingPong("Pong");
+    public Pingpong getPingPong(){
+        return new Pingpong("Pong");
     }
 }
